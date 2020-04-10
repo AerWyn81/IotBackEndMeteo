faeb6d55ba0d7dbf38ce2a6c8d206443a9bc3d19
Damien <damien.madaule@ynov.com>
2020-04-10 16:15:46 +0200

    API Function to get data from one device
    Add :
     - Add Route /device/:deviceName
     - Add Api/InfoDevice

---
+++ src/api/InfoDevice.js
@@ -0,0 +1,16 @@
+import Device from "../models/Device";
+const infoDevice = async  (req, res) => {
+    const deviceName = req.params.deviceName;
+    let device = await Device.findByName(deviceName);
+    if (!device) {
+        return res.status(404).json({ error: "Device not found" });
+    }
+    let zoneLat = device.zoneLat;
+    let zoneLong = device.zoneLong;
+    let temperature = device.temperature;
+    let wind = device.windSpeed;
+    let humidity = device.humidity;
+    res.status(200).json({zoneLat:zoneLat,zoneLong:zoneLong,temperature:temperature,wind: wind,humidity: humidity});
+};
+
+export default infoDevice
--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,10 @@
 import express from "express"
 import home from "./home"
+import infoDevice from "./infoDevice"
 
 const router = express.Router();
 
 router.get("/", home);
+router.get("/device/:deviceName", infoDevice);
 
 export default router
