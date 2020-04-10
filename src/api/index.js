c77d21a462d5d52d6d0e2b6066e79c45ae3169ee
Damien <damien.madaule@ynov.com>
2020-04-10 17:25:53 +0200

    API Endpoint to get Average from one device for Temp,Wind,Humidity
      Add :
     - Add Route /device/:deviceName/average
     - Add Api/AverageDevice

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,13 @@ import express from "express"
 import home from "./home"
 import temperature from "./temperature"
 import infoDevice from "./infoDevice"
+import AverageDevice from "./AverageDevice"
 
 const router = express.Router();
 
 router.get("/", home);
 router.get("/device/:deviceName", infoDevice);
 router.get("/devices/:deviceName/temperature", temperature);
+router.get("/device/:deviceName/average", AverageDevice);
 
 export default router
