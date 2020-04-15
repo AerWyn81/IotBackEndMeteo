f3c0c2fb16f449a528ce832b6724fb699dfe132c
Pierre <pierre.bourges@ynov.com>
2020-04-15 20:09:54 +0200

    Docker mongo api
    Add :
    - Connect with retry for mongo in api
    
    Change :
    - Docker-compose api build location
    - DockerFile api expose port 9000 in place of 80
    
    Remove :
    - Docker-compose mongo volumes and command
    - Set port for windows only in api package
    - MongoInstance export

--- api/database/mongodb.js
+++ api/database/mongodb.js
@@ -1,9 +1,25 @@
 const mongoose = require("mongoose");
 
-const connectionUri = "mongodb://localhost:27017/weather";
-const mongoInstance = mongoose.connect(connectionUri, {
+const connectionUri = "mongodb://mongodb:27017/weather";
+
+const options = {
+  autoIndex: false, // Don't build indexes
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0,
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+};
 
-module.exports = mongoInstance;
+const connectWithRetry = () => {
+  console.log('MongoDB connection with retry')
+  mongoose.connect(connectionUri, options).then(()=>{
+    console.log('MongoDB is connected')
+  }).catch(err=>{
+    console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
+    setTimeout(connectWithRetry, 5000)
+  })
+};
+
+connectWithRetry();
+
