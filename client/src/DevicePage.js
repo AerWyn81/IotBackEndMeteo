d196c2008283e7425701e12176c0632e9d4257c2
Jartek <aurelien.bouteiller@ynov.com>
2020-04-16 19:05:33 +0200

    Charts
    Add:
    - react-charts lib to show device data
    - Chart with temperature, humidity, windSpedd and status for the specified device
    - Navbar in top of specific device
    
    Change:
    - Size and position of table 
    - status label in the table

--- client/src/DevicePage.js
+++ client/src/DevicePage.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { Chart } from "react-charts";
 
-const {useEffect} = require("react");
-
-
+const { useEffect } = require("react");
 
 function DevicePage() {
   const { device } = useParams();
@@ -12,7 +11,7 @@ function DevicePage() {
 
   const getDataFromApi = async (device) => {
     const { data } = await axios.get(
-        `http://localhost:9000/api/v1/devices/${device}`
+      `http://localhost:9000/api/v1/devices/${device}`
     );
     setDeviceData(data);
   };
@@ -21,27 +20,80 @@ function DevicePage() {
     getDataFromApi(device);
   }, []);
 
+  const temperatureData = deviceData.map((data, index) => [
+    index,
+    data.temperature,
+  ]);
+
+  const humidityData = deviceData.map((data, index) => [index, data.humidity]);
+
+  const windSpeedData = deviceData.map((data, index) => [
+    index,
+    data.windSpeed,
+  ]);
+
+  const data = useMemo(
+    () => [
+      {
+        label: "Temperature",
+        data: temperatureData,
+      },
+      {
+        label: "Humidity",
+        data: humidityData,
+      },
+      {
+        label: "Wind Speed",
+        data: windSpeedData,
+      },
+    ],
+    [deviceData]
+  );
+
+  const axes = useMemo(
+    () => [
+      { primary: true, type: "linear", position: "bottom" },
+      { type: "linear", position: "left" },
+    ],
+    []
+  );
+
   return (
     <div>
-      <h1>{device}</h1>
-      <a href="/" className="btn btn-outline-dark btn-block" style={{width:"100px"}}>
-        Home
-      </a>
+      <div className="navbar navbar-light bg-light">
+        <a href="/" className="navbar-toggler" type="button">
+          Home
+        </a>
+        <h1 className="navbar-brand">{device}</h1>
+      </div>
 
-      <table class="table">
-        <tr>
-          <th>temperature</th>
-          <th>humidity</th>
-          <th>status</th>
-          <th>windSpeed</th>
-        </tr>
-        { deviceData.map(item =><tr>
-          <td>{item.temperature}</td>
-          <td>{item.humidity}</td>
-          <td>{item.status ? "O" : "X"}</td>
-          <td>{item.windSpeed}</td>
-        </tr>)}
-      </table>
+      <div className="d-flex justify-content-lg-center">
+        <table className="table col-lg-8">
+          <thead>
+            <tr>
+              <th>temperature</th>
+              <th>humidity</th>
+              <th>windSpeed</th>
+              <th>status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {deviceData.map((item) => (
+              <tr>
+                <td>{item.temperature} °C</td>
+                <td>{item.humidity} %</td>
+                <td>{item.windSpeed} Km/h</td>
+                <td>{item.status ? "Actif" : "Inactif"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="d-flex justify-content-lg-center">
+        <div className="col-lg-10" style={{ height: "500px" }}>
+          <Chart data={data} axes={axes} tooltip />
+        </div>
+      </div>
     </div>
   );
 }
