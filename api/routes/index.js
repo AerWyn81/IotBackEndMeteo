6f1d241e9fe9d5f62bf5144c0036467a82db8c2e
Jartek <aurelien.bouteiller@ynov.com>
2020-04-16 12:24:06 +0200

    Device info values
    Add:
    - route /devices/:deviceName/last to return the last values of the specific device
    
    Change:
    - route /devices/:deviceName return the 20 last values of the specific device
    
    Fix:
    - Device model default date

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,7 @@ const convertTemperature = require("./convertTemperature");
 const humidity = require("./humidity");
 const unknown = require("./unknown");
 const infoDevice = require("./infoDevice");
+const allInfoDevice = require("./allInfoDevice");
 const averageDevice = require("./averageDevice");
 const windSpeed = require("./windSpeed");
 const temperature = require("./temperature");
@@ -11,7 +12,8 @@ const temperature = require("./temperature");
 const router = express.Router();
 
 router.get("/", home);
-router.get("/devices/:deviceName", infoDevice);
+router.get("/devices/:deviceName", allInfoDevice);
+router.get("/devices/:deviceName/last", infoDevice);
 router.get("/devices/:deviceName/convertTemperature", convertTemperature);
 router.get("/devices/:deviceName/average", averageDevice);
 router.get("/devices/:deviceName/humidity", humidity);
